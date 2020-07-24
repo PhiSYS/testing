2569c632763a4dc97a5dcb99fa429b4a674fbc0c
Jose Flores <jr.flores@dosfarma.com>
2020-07-24 08:29:15 +0200

    return rabbit messages

--- src/Behaviour/AMQP/RabbitMQManager.php
+++ src/Behaviour/AMQP/RabbitMQManager.php
@@ -3,41 +3,60 @@ declare(strict_types=1);
 
 namespace DosFarma\Testing\Behaviour\AMQP;
 
-use PhpAmqpLib\Channel\AMQPChannel;
 use PhpAmqpLib\Connection\AMQPStreamConnection;
-use PhpAmqpLib\Exchange\AMQPExchangeType;
+use PhpAmqpLib\Exception\AMQPTimeoutException;
+use PhpAmqpLib\Message\AMQPMessage;
 
 final class RabbitMQManager implements AmqpManager
 {
     private AMQPStreamConnection $connection;
-    private AMQPChannel $channel;
 
-    public function __construct(string $host, int $port, string $user, string $pass)
+    public function __construct(
+        string $host,
+        int $port,
+        string $user,
+        string $pass
+    )
     {
         $this->connection = new AMQPStreamConnection($host, $port, $user, $pass);
-        $this->channel = $this->connection->channel();
     }
 
-    public function consume(string $queue, string $exchange)
+    public function consume(string $queue, string $exchange): array
     {
-        $callback = function($msg){
-            echo " [x] Received ", $msg->body, "\n";
-            $job = \json_decode($msg->body, $assocForm=true);
-            sleep($job['sleep_period']);
-            echo " [x] Done", "\n";
-            $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
-        };
+        $channel = $this->connection->channel();
+        $messages = [];
 
-        //$this->channel->basic_qos(null, 1, null);
-        $this->channel->queue_declare($queue, false, false, true, true);
-        $this->channel->exchange_declare($exchange, AMQPExchangeType::TOPIC, false, true, false);
+        $channel->basic_consume(
+            $queue,
+            '',
+            true,
+            false,
+            false,
+            false,
+            static function (AMQPMessage $message) use (&$messages)
+            {
+                \array_push(
+                    $messages,
+                    \json_decode(
+                        $message->body,
+                        true)
+                );
+            }
+        );
 
-        $this->channel->queue_bind($queue, $exchange);
+        while ($channel->is_consuming()) {
+            try {
+                $channel->wait(null, false, 2);
+            } catch (AMQPTimeoutException $exception)
+            {
+                break;
+            }
+        }
 
-        $this->channel->basic_consume($queue, false, true, false, $callback);
-
-        $this->channel->close();
+        $channel->close();
         $this->connection->close();
+
+        return $messages;
     }
 }
 
