d4114594336460e54ee605fd8bc53056c420eb05
Sergio Hernández Martínez <hm.sergio@gmail.com>
2020-09-04 08:27:17 +0200

    Added method to clear responses.

--- src/Behaviour/ApiRest/ApiCallsManager.php
+++ src/Behaviour/ApiRest/ApiCallsManager.php
@@ -7,14 +7,16 @@ use Psr\Http\Message\ResponseInterface;
 
 interface ApiCallsManager
 {
+    /**
+     * @return ResponseInterface[]
+     */
+    public function responses(): array;
+
+    public function clearResponses(): void;
+
     public function post(string $uriPath, array $body, array $uriVariables = []): void;
 
     public function put(string $uriPath, array $body, array $uriVariables = []): void;
 
     public function delete(string $uriPath, array $uriVariables = []): void;
-
-    /**
-     * @return ResponseInterface[]
-     */
-    public function responses(): array;
 }
