6692dca80710e46173b09b4926237cc5147f0b63
Jose Flores <jr.flores@dosfarma.com>
2020-12-03 23:01:07 +0100

    serialization method

--- src/Behaviour/Bus/BusManager.php
+++ src/Behaviour/Bus/BusManager.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace DosFarma\Testing\Behaviour\Bus;
 
 use PcComponentes\Ddd\Util\Message\AggregateMessage;
+use PcComponentes\Ddd\Util\Message\Message;
 use PcComponentes\Ddd\Util\Message\SimpleMessage;
 
 interface BusManager
@@ -19,4 +20,6 @@ interface BusManager
     public function handleCommand(array $commandDatum): void;
 
     public function deserializeCommand(array $commandDatum): SimpleMessage;
+
+    public function serializeMessage(Message $message);
 }
